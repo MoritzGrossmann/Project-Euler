5970de3e30dc24d5d596f07064928afa7a7ea00d
Moritz Großmann <moritz.grossmann95@googlemail.com>
2017-07-27 21:22:02 +0200

    Problem 33 solved

--- 33 Digit cancelling fractions/Fraction.java
+++ 33 Digit cancelling fractions/Fraction.java
@@ -1,44 +1,92 @@
 public class Fraction {
 
-    private int numerator;
+    public int numerator;
 
-    private int denominator;
-
-    private int match;
-
+    public int denominator;
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
-    public boolean hasSameDigit() {
-
-        if (numerator % 10 == 0 || denominator % 10 == 0) {
+    public boolean isCurious() {
+        if         (numerator % 10 == denominator % 10) {
+            return new Fraction(this.numerator / 10, this.denominator / 10).equals(this); 
+        } else if  (numerator % 10 == denominator / 10) {
+            return new Fraction(this.numerator / 10, this.denominator % 10).equals(this); 
+        } else if  (numerator / 10 == denominator % 10) {
+            return new Fraction(this.numerator % 10, this.denominator / 10).equals(this); 
+        } else if  (numerator / 10 == denominator / 10) {
+           return new Fraction(this.numerator % 10, this.denominator % 10).equals(this); 
+        } else {
             return false;
         }
-        if (numerator % 10 == denominator % 10 || numerator % 10 == denominator / 10) {
-            this.match = numerator % 10;
-        } else {
-            if (numerator / 10 == denominator % 10 || numerator / 10 == denominator / 10) {
-                this.match = numerator / 10;
-            }
-        }
-
-        return false;
     }
 
-    public boolean isCurious() {
-        if (numerator % 10 == denominator % 10) {
-            return ((numerator / 10) / (denominator /10)) == numerator / denominator; 
-        } else if (numerator % 10 == denominator / 10){
-            return ((numerator / 10) / (denominator % 10)) == numerator / denominator; 
-        } else if (numerator / 10 == denominator % 10){
-            return ((numerator % 10) / (denominator / 10)) == numerator / denominator; 
-        } else if (numerator / 10 == denominator / 10){
-            return ((numerator % 10) / (denominator % 10)) == numerator / denominator; 
-        } else {
-            return false;
+    private boolean isZero() {
+        return this.numerator == 0 && this.denominator == 0;
+    }
+
+    public boolean isTrivial() {
+        return (this.numerator % 10 == 0 && this.denominator % 10 == 0) || this.numerator == this.denominator;
+    }
+
+    public boolean equals(Fraction f) {
+        return this.numerator * f.denominator == this.denominator * f.numerator;
+    }
+
+    private boolean sameDigits(Fraction f) {
+        return this.numerator == f.numerator && this.denominator == f.denominator;
+    }
+
+    public Fraction add(Fraction f) {
+
+        if (f.isZero()) {
+            return this;
         }
-    }    
+
+        if (this.isZero()) {
+            return f;
+        }
+        
+        return new Fraction(this.numerator * f.denominator + f.numerator * this.denominator, this.denominator * f.denominator).shortening();
+    }
+
+    public Fraction substract(Fraction f) {
+
+        if (f.isZero()) {
+            return this;
+        }
+
+        if (this.isZero()) {
+            return f;
+        }
+
+        return new Fraction(this.numerator * f.denominator - f.numerator * this.denominator, this.denominator * f.denominator).shortening();
+    }
+
+    public Fraction multiply(Fraction f) {
+        return new Fraction(this.numerator * f.numerator, this.denominator * f.denominator).shortening();
+    }
+
+    public Fraction divide(Fraction f) {
+        return new Fraction(this.numerator * f.denominator, this.denominator * f.numerator).shortening();
+    }
+
+    public Fraction shortening() {
+        for (int i = denominator / 2; i > 0; i--) {
+            if (denominator % i == 0 && numerator % i == 0) {
+                return new Fraction(numerator / i, denominator /i);
+            }
+        }
+        return this;
+    }
+
+    public String toString() {
+        return numerator+"/"+denominator;
+    }
+
+    public void print() {
+        System.out.println(this.toString());
+    }
 }
---
+++ 33 Digit cancelling fractions/Program.java
@@ -0,0 +1,19 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+        Fraction sum = new Fraction(1,1);
+
+        for (int i = 10; i < 100; i++) {
+            for (int j = 10; j < i; j++) {
+                Fraction f = new Fraction(j, i);
+                if (f.isCurious() && !f.isTrivial()) {
+                    sum = sum.multiply(f);
+                    f.print();
+                }
+            }
+        }
+
+        System.out.println("Summe: " + sum.shortening());
+    }
+}
